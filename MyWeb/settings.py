7b03480b60f84932ca1a11537504cc1a275afaa2
Huzzy619 <blazingkrane@gmail.com>
2023-02-09 05:22:49 +0100

    more files

--- MyWeb/settings.py
+++ MyWeb/settings.py
@@ -162,3 +162,40 @@ LOGIN_REDIRECT_URL = "index"
 #     'API_KEY': config('CLOUD_API_KEY'),
 #     'API_SECRET': config('CLOUD_API_SECRET'),
 # }
+
+JAZZMIN_SETTINGS = {
+     # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Mine",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Portfolio",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "HUZZY",
+
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    "site_logo": "images/apple-touch-icon.png",
+
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": "images/apple-touch-icon.png",
+
+    # Logo to use for login form in dark themes (defaults to login_logo)
+    "login_logo_dark": None,
+
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": "images/favicon-32x32.png",
+
+    # Welcome text on the login screen
+    "welcome_sign": "Login to Huzzy's Portfolio Website",
+
+    # Copyright on the footer
+    "copyright": "HUZZY",
+
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "theme": "united",
+}
