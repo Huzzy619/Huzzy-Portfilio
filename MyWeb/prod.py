0b2717584d60a348f7e2c1f4bc28bbdfeceae515
Huzzy619 <blazingkrane@gmail.com>
2023-05-01 23:36:22 +0100

    redeploy May

--- MyWeb/prod.py
+++ MyWeb/prod.py
@@ -26,5 +26,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ['CLOUD_NAME'],
     'API_KEY': os.environ['CLOUD_API_KEY'],
-    'API_SECRET': os.environ['CLOUD_API_SECRET'],
+    'API_SECRET': os.environ['CLOUD_API_SECRET'], 
 }
